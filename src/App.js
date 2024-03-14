cb0c35441661fae94a34e6a9c7ebd832a9121c6d
Sarthak Niranjan <22b3923@iitb.ac.in>
2024-03-15 01:03:54 +0530

    initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div className="container">
+      {/* <Header /> */}
+      <Home />
+    </div>
+  );
+}
+
+export default App;
